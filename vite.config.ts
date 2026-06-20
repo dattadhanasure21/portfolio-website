34d3d2ba36125f45fa0e638b7f5bd4026f3fed62
dattadhanasure21 <dattadhanasure21@gmail.com>
2026-06-21 01:30:00 +0530

    Fix base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
-  base: "/portfolio-website/",
-
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
